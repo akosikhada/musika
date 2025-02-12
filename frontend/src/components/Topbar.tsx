e23a2ef9a91f3bb83fed732324bd6e993d4af036
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-02-12 19:07:52 +0800

    fix(Topbar): update component imports and admin link visibility
    - Replaced 'SignedOut, SignedIn, SignOutButton' imports with 'SignedOut, UserButton'
    - Added 'useAuthStore' to determine admin status and show admin link conditionally
    - Updated class names for styling consistency

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,28 +1,35 @@
-import { SignedOut, SignedIn, SignOutButton } from "@clerk/clerk-react";
+import { SignedOut, UserButton } from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButtons from "./SignInOAuthButtons";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
 
 const Topbar = () => {
-  const isAdmin = false;
+  const { isAdmin } = useAuthStore();
   return (
     <div className="flex  items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
-      <div className="flex gap-2 items-center">Musika</div>
+      <div className="flex gap-2 items-center">
+        <img src="/favicon.ico" alt="Musika Logo" className="size-8" />
+        Musika
+      </div>
       <div className="flex gap-4 items-center">
         {isAdmin && (
-          <Link to={"/admin"}>
+          <Link
+            to={"/admin"}
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
             <LayoutDashboardIcon className="size-4 mr-2" />
             Admin Dashboard
           </Link>
         )}
 
-        <SignedIn>
-          <SignOutButton />
-        </SignedIn>
-
         <SignedOut>
           <SignInOAuthButtons />
         </SignedOut>
+
+        <UserButton />
       </div>
     </div>
   );
